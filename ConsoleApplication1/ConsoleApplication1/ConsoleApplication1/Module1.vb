c148bcaf929248704bac83eb847bda09c691f8f7
syedakhtar94 <akhtar_mudassar@yahoo.com>
2019-04-24 08:32:07 -0400

    first program using operators

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim X As Double
+        X = Console.ReadLine()
+        Dim Y As Double
+        Y = Console.ReadLine()
+        Console.WriteLine("Integer Part of X = {0}", X \ 1)
+        Console.WriteLine("Integer Part of Y = {0}", Y \ 1)
+        Console.WriteLine("Fraction Part of X = {0}", X - (X \ 1))
+        Console.WriteLine("Fraction Part of Y = {0}", Y - (Y \ 1))
+        Console.WriteLine("X - Y = {0}", X - Y)
+        Console.WriteLine("X + Y = {0}", X + Y)
+        Console.WriteLine("X * Y = {0}", X * Y)
+        Console.WriteLine("X / Y = {0}", X / Y)
+        Console.WriteLine(X / 0) 'Modulus with zero gives Infinity '
+        'Console.WriteLine(X \ 0) Throws an exception
+    End Sub
+
+End Module
